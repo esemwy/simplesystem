63787853faddf57ca9cc1bd816e4679ae28053a7
Esemwy <github.5.smy@neverbox.com>
2022-12-06 15:38:11 -0500

    Fresh Start

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SS = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ SS.abilities = {
+  "str": "SS.AbilityStr",
+  "dex": "SS.AbilityDex",
+  "con": "SS.AbilityCon",
+  "int": "SS.AbilityInt",
+  "wis": "SS.AbilityWis",
+  "cha": "SS.AbilityCha"
+};
+
+SS.abilityAbbreviations = {
+  "str": "SS.AbilityStrAbbr",
+  "dex": "SS.AbilityDexAbbr",
+  "con": "SS.AbilityConAbbr",
+  "int": "SS.AbilityIntAbbr",
+  "wis": "SS.AbilityWisAbbr",
+  "cha": "SS.AbilityChaAbbr"
+};
